c186ea20027e7fccbbe5ea8b78576e803b3bef8d
doohwancho <doohwancho526@gmail.com>
2021-09-18 10:27:11 +0900

    0.1.1 +: hide page until page load completely

--- background.js
+++ background.js
@@ -34,7 +34,11 @@ communityKeywordList = [
 	'fomos'
 ];
 
+let browser;
 
+if (typeof browser === "undefined") {
+    browser = chrome;
+}
 
 
 if (!localStorage.on) {
@@ -42,35 +46,40 @@ if (!localStorage.on) {
 }
 
 if (localStorage.on == '1') {
-	chrome.browserAction.setIcon({path: "images/icon19.png"});
+	browser.browserAction.setIcon({path: "images/icon19.png"});
 } else {
-	chrome.browserAction.setIcon({path: "images/icon19-disabled.png"});
+	browser.browserAction.setIcon({path: "images/icon19-disabled.png"});
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+browser.browserAction.onClicked.addListener(function(tab) {
 	if (localStorage.on == '1') {
-		chrome.browserAction.setIcon({path: "images/icon19-disabled.png"});
+		browser.browserAction.setIcon({path: "images/icon19-disabled.png"});
 		localStorage.on = '0';
 	} else {
-		chrome.browserAction.setIcon({path: "images/icon19.png"});
+		browser.browserAction.setIcon({path: "images/icon19.png"});
 		localStorage.on = '1';
 	}
 });
 
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
+browser.webRequest.onBeforeRequest.addListener(function(details) {
 	if (localStorage.on == '1') {
 		return {redirectUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="}; 
 	}
 }, { urls: communitySites, types: ["image", "object", "media", "sub_frame"]}, ["blocking"]);  //stylesheet
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (localStorage.on == '1'){
 		communityKeywordList.forEach( function(s) { 
 			if (tab.url.indexOf(s) > -1) {
-				chrome.tabs.insertCSS(null, {code: "img{display: none; visibility: hidden;} video{display: none}", runAt: "document_start"}); 
-				chrome.tabs.executeScript(null, {code: "document.querySelectorAll('video').forEach(video => video.remove()); document.querySelectorAll('iframe').forEach(iframe => iframe.remove()); document.querySelectorAll('embed').forEach(embed => embed.remove());", runAt: "document_end"});   
+				if ( changeInfo.status === "loading" ){
+					browser.tabs.insertCSS(null, {code: "html{display:none;}", runAt: "document_start"}); 
+				}
+				if ( changeInfo.status === "complete" ){
+					browser.tabs.insertCSS(null, {code: "html{display:block;} img{display: none; visibility: hidden;} video{display: none} "}); 
+				}
+				browser.tabs.executeScript(null, {code: "document.querySelectorAll('video').forEach(video => video.remove()); document.querySelectorAll('iframe').forEach(iframe => iframe.remove()); document.querySelectorAll('embed').forEach(embed => embed.remove());", runAt: "document_end"});   
 				return;
 			}
 		})
