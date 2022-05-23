309478601e0e2a26a23870b47ccdd15d6c6468d8
Doohwancho <doohwancho526@gmail.com>
2022-05-23 22:18:21 +0900

    0.2.5 [refactor] background.js

--- src/background.js
+++ src/background.js
@@ -1,143 +1,148 @@
-let communitySites = [
-  "*://*.dcinside.com/*",
-  "*://*.fmkorea.com/*",
-  "*://*.ilbe.com/*",
-  "*://*.ruliweb.com/*",
-  "*://*.ppomppu.co.kr/*",
-  "*://*.inven.co.kr/*",
-  "*://*.theqoo.net/*",
-  "*://*.clien.net/*",
-  "*://*.pann.nate.com/*",
-  "*://*.humoruniv.com/*",
-  "*://mlbpark.donga.com/*",
-  "*://*.instragram.com/*",
-  "*://*.facebook.com/*",
-  "*://*.fomos.kr/*",
-];
+let obj = {};
 
-let communityKeywordList = [
-  "dcinside.",
-  "fmkorea.",
-  "ilbe.",
-  "ruliweb.",
-  "ppomppu.",
-  "inven.",
-  "theqoo.",
-  "clien.",
-  "pann.",
-  "nate.",
-  "humoruniv.",
-  "mlbpark.",
-  "donga.",
-  "instagram.",
-  "facebook.",
-  "fomos.",
-];
 
-let browser;
+function main(){
 
-if (typeof browser === "undefined") {
-  browser = window.chrome;
+  setBrowserToChrome();
+  setCommunitySites();
+  setCommunityKeyWordList();
+
+  setToggle();
+  
+  blockImageBeforeRequest();
+  removeDistractionOnUpdate();
 }
 
-if (!localStorage.on) {
-  localStorage.on = "1";
+
+
+
+
+//* functions *//
+
+function setBrowserToChrome(){
+  if (typeof obj.browser === "undefined") {
+    obj.browser = window.chrome;
+  }
 }
 
-if (localStorage.on == "1") {
-  browser.browserAction.setIcon({ path: "images/icon19.png" });
-} else {
-  browser.browserAction.setIcon({ path: "images/icon19-disabled.png" });
+function setCommunitySites(){
+  obj.communitySites = [
+    "*://*.dcinside.com/*",
+    "*://*.fmkorea.com/*",
+    "*://*.ilbe.com/*",
+    "*://*.ruliweb.com/*",
+    "*://*.ppomppu.co.kr/*",
+    "*://*.inven.co.kr/*",
+    "*://*.theqoo.net/*",
+    "*://*.clien.net/*",
+    "*://*.pann.nate.com/*",
+    "*://*.humoruniv.com/*",
+    "*://mlbpark.donga.com/*",
+    "*://*.instragram.com/*",
+    "*://*.facebook.com/*",
+    "*://*.fomos.kr/*",
+  ];
 }
 
-browser.browserAction.onClicked.addListener(function () {
-  if (localStorage.on == "1") {
-    browser.browserAction.setIcon({ path: "images/icon19-disabled.png" });
-    localStorage.on = "0";
-  } else {
-    browser.browserAction.setIcon({ path: "images/icon19.png" });
+function setCommunityKeyWordList(){
+  obj.communityKeywordList = [
+    "dcinside.",
+    "fmkorea.",
+    "ilbe.",
+    "ruliweb.",
+    "ppomppu.",
+    "inven.",
+    "theqoo.",
+    "clien.",
+    "pann.",
+    "nate.",
+    "humoruniv.",
+    "mlbpark.",
+    "donga.",
+    "instagram.",
+    "facebook.",
+    "fomos.",
+  ];
+}
+
+function setToggle(){
+  if (!localStorage.on) {
     localStorage.on = "1";
   }
-});
-
-browser.webRequest.onBeforeRequest.addListener(
-  function () {
+  
+  if (localStorage.on == "1") {
+    obj.browser.browserAction.setIcon({ path: "images/icon19.png" });
+  } else {
+    obj.browser.browserAction.setIcon({ path: "images/icon19-disabled.png" });
+  }
+  
+  obj.browser.browserAction.onClicked.addListener(function () {
     if (localStorage.on == "1") {
-      return {
-        redirectUrl:
-          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==",
-      };
+      obj.browser.browserAction.setIcon({ path: "images/icon19-disabled.png" });
+      localStorage.on = "0";
+    } else {
+      obj.browser.browserAction.setIcon({ path: "images/icon19.png" });
+      localStorage.on = "1";
     }
-  },
-  { urls: communitySites, types: ["image", "object", "media", "sub_frame"] },
-  ["blocking"]
-); //stylesheet
+  });
+}
+
+function blockImageBeforeRequest(){
+  obj.browser.webRequest.onBeforeRequest.addListener(
+    function () {
+      if (localStorage.on == "1") {
+        return {
+          redirectUrl:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==",
+        };
+      }
+    },
+    { urls: obj.communitySites, types: ["image", "object", "media", "sub_frame"] },
+    ["blocking"]
+  ); 
+}
+
+function removeDistractionOnUpdate(){
+  obj.browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    removeDistractions(tabId, changeInfo.status, tab.url);
+  });
+}
 
 function removeDistractions(tabId, status, url) {
   if (localStorage.on == "1") {
-    communityKeywordList.forEach(function (s) {
+    obj.communityKeywordList.forEach(function (s) {
       if (url.indexOf(s) > -1) {
         if (status === "loading") {
-          browser.tabs.insertCSS(
-            tabId,
-            {
-              file: "src/block_style.css",
-              allFrames: true,
-              runAt: "document_start",
-            },
-            function () {
-              browser.tabs.executeScript({
-                code: "document.documentElement.classList.add('black-and-white-mode');",
-                allFrames: true,
-                runAt: "document_start",
-              });
-            }
-          );
+            insertCss(tabId);
+            addCssTag();
         }
-        browser.tabs.executeScript(null, {
-          code: "document.querySelectorAll('video').forEach(video => video.remove()); document.querySelectorAll('iframe').forEach(iframe => iframe.remove()); document.querySelectorAll('embed').forEach(embed => embed.remove()); document.getElementsByTagName('html')[0].style.display='block';",
-          runAt: "document_end",
-        });
-        return;
+        removeVideo();
       }
     });
   }
 }
 
-browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  removeDistractions(tabId, changeInfo.status, tab.url);
-});
-
-browser.tabs.onCreated.addListener(function (tab) {
-  if (localStorage.on == "1") {
-    communityKeywordList.forEach(function (s) {
-      if (tab.pendingUrl.indexOf(s) > -1) {
-        browser.tabs.insertCSS(tab.id, {
-          file: "src/block_style.css",
-          allFrames: true,
-          runAt: "document_start",
-        });
-      }
-    });
-  }
-});
-
-browser.tabs.onActivated.addListener(function (activeInfo) {
-  browser.tabs.get(activeInfo.tabId, function (tab) {
-    if (localStorage.on == "1") {
-      communityKeywordList.forEach(function (s) {
-        if (tab.url.indexOf(s) > -1) {
-          browser.tabs.executeScript({
-            code: "document.documentElement.classList.add('black-and-white-mode');",
-            allFrames: true,
-          });
-
-          browser.tabs.executeScript(null, {
-            code: "document.querySelectorAll('video').forEach(video => video.remove()); document.querySelectorAll('iframe').forEach(iframe => iframe.remove()); document.querySelectorAll('embed').forEach(embed => embed.remove()); document.getElementsByTagName('html')[0].style.display='block';",
-            runAt: "document_end",
-          });
-        }
-      });
-    }
+function insertCss(tabId){
+  obj.browser.tabs.insertCSS(tabId, {
+    file: "src/block_style.css",
+    allFrames: true,
+    runAt: "document_start",
   });
-});
+}
+
+function addCssTag(){
+  obj.browser.tabs.executeScript({
+    code: "document.documentElement.classList.add('black-and-white-mode');",
+    allFrames: true,
+  });
+}
+
+function removeVideo(){
+  obj.browser.tabs.executeScript(null, {
+    code: "document.querySelectorAll('video').forEach(video => video.remove()); document.querySelectorAll('iframe').forEach(iframe => iframe.remove()); document.querySelectorAll('embed').forEach(embed => embed.remove()); document.getElementsByTagName('html')[0].style.display='block';",
+    runAt: "document_end",
+  });
+}
+
+
+main();
